3082763949cb4a1a18784963785cbcaf419ed20c
Israa Irshad <israa@qburst.com>
2023-10-03 17:09:24 +0530

    Added dummy data

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts =[{
+    'author': 'Israa',
+    'title' : 'First Post',
+    'date'  : 'Oct 3, 2023',
+    'content': 'First Try with dummy data'
+    },
+    {
+    'author': 'Rohin',
+    'title' : 'Second Post',
+    'date'  : 'Oct 3, 2023',
+    'content': 'Its good to do Django'
+    }
+]
+
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
     return render(request, 'blog/about.html')
